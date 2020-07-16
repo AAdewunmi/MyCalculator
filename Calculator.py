f1ecf2db17224d4c02f2a0f6df1de241a1041301
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-07-16 15:45:13 +0100

    Third Commit: Adding conditional statements

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,31 @@
-# Prompt Users for Input
+# TODO
+# 1 Enter valid number
+# ** for power, % for modulo functionality
+# 3 Square root functionality
 # ***********************
+
 number1 = int(input('Please enter first number: '))
 number2 = int(input('Please enter second number: '))
-# Adding Operators "+", "-", "/", "*"
-# ***********************
-# Addition
-print('{} + {} = '.format(number1, number2))
-print(number1 + number2)
-# Subtraction
-print('{} - {} = '.format(number1, number2))
-print(number1 - number2)
-# Multiplication
-print('{} * {} = '.format(number1, number2))
-print(number1 * number2)
-# Division
-print('{} / {} = '.format(number1, number2))
-print(number1 / number2)
+
+operation = input('''
+Select Mathematical Operation:
++ for addition
+- for subtraction
+* for multiplication 
+% for division\n
+''')
+
+if operation == '+':
+    print('\n{} + {} = '.format(number1, number2))
+    print(number1 + number2)
+elif operation == '-':
+    print('\n{} - {} = '.format(number1, number2))
+    print(number1 - number2)
+elif operation == '*':
+    print('\n{} * {} = '.format(number1, number2))
+    print(number1 * number2)
+elif operation == '%':
+    print('\n{} % {} = '.format(number1, number2))
+    print(number1 % number2)
+else:
+    print('Invalid Operator, Try Again')

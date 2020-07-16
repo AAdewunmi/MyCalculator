62c1406906cdb155e6ad4fd0c1446fcb9b6e1a1c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-07-16 14:28:37 +0100

    Second Commit: Adding conditional statements

--- Calculator.py
+++ Calculator.py
@@ -2,5 +2,17 @@
 # ***********************
 number1 = int(input('Please enter first number: '))
 number2 = int(input('Please enter second number: '))
-addition = number1 + number2
-print(addition)
+# Adding Operators "+", "-", "/", "*"
+# ***********************
+# Addition
+print('{} + {} = '.format(number1, number2))
+print(number1 + number2)
+# Subtraction
+print('{} - {} = '.format(number1, number2))
+print(number1 - number2)
+# Multiplication
+print('{} * {} = '.format(number1, number2))
+print(number1 * number2)
+# Division
+print('{} / {} = '.format(number1, number2))
+print(number1 / number2)

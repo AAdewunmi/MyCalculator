056fca0b6a3a3a3bb1883094a0be61264e45259c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-07-16 13:48:44 +0100

    First Commit: Prompt user for input

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+# Prompt Users for Input
+# ***********************
+number1 = int(input('Please enter first number: '))
+number2 = int(input('Please enter second number: '))
+addition = number1 + number2
+print(addition)
